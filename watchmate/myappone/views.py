591b3f8b8d33c0ebcfb2354dd5d0bb2e53860009
nilesh8599 <139100479+nilesh8599@users.noreply.github.com>
2023-08-20 16:31:01 +0530

    Commit:12

--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , JsonResponse
 from myappone.models import Movie
+
+
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     return HttpResponse(movies.values())
+
 
 def movie_list(request):
     movies = Movie.objects.all()
-    return HttpResponse(movies.values())
+
+    data ={
+        'movies':list(movies.values())
+    }
+
+    return JsonResponse(data)
 
