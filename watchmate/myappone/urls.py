537abde4838486c7d36a18633a2571cf703955c8
nilesh8599 <139100479+nilesh8599@users.noreply.github.com>
2023-08-26 15:33:18 +0530

    commit:14

--- watchmate/myappone/urls.py
+++ watchmate/myappone/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path 
-from myappone import views
+# from django.urls import path 
+# from myappone import views
 
-urlpatterns = [
-    path('list/' , views.movie_list , name ='movie-list'),
-    path('<int:pk>/' , views.movie_details , name ='movie_details'),
-]
+# urlpatterns = [
+#     path('list/' , views.movie_list , name ='movie-list'),
+#     path('<int:pk>/' , views.movie_details , name ='movie_details'),
+# ]
--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse , JsonResponse
-from myappone.models import Movie
+# from django.shortcuts import render
+# from django.http import HttpResponse , JsonResponse
+# from myappone.models import Movie
 
 
 # def movie_list(request):
@@ -8,22 +8,22 @@ from myappone.models import Movie
 #     return HttpResponse(movies.values())
 
 
-def movie_list(request):
-    movies = Movie.objects.all()
+# def movie_list(request):
+#     movies = Movie.objects.all()
 
-    data ={
-        'movies':list(movies.values())
-    }
+#     data ={
+#         'movies':list(movies.values())
+#     }
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
-def movie_details(request , pk):
-    movie = Movie.objects.get(pk=pk)
+# def movie_details(request , pk):
+#     movie = Movie.objects.get(pk=pk)
 
-    data={
-        'movie':movie.name,
-        'description':movie.description,
-        'active':movie.active
-    }
+#     data={
+#         'movie':movie.name,
+#         'description':movie.description,
+#         'active':movie.active
+#     }
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
